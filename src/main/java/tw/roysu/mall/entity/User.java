05333e6cf6baf117d8db3f132b67aed0113f95b2
RoySu1215 <roysu1215@gmail.com>
2015-08-01 00:41:59 +0800

    User Entity

---
+++ src/main/java/tw/roysu/mall/entity/User.java
@@ -0,0 +1,85 @@
+package tw.roysu.mall.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * 會員
+ */
+@Entity
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	/** 
+	 * 會員編號
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	/** 
+	 * 帳號
+	 */
+	private String account;
+
+	/** 
+	 * 密碼
+	 */
+	private String password;
+
+	/** 
+	 * 信箱
+	 */
+	private String email;
+
+	/** 
+	 * 暱稱
+	 */
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
