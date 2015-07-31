a85e38f869750304ee0dc24f3ebe699cea6958e6
RoySu1215 <roysu1215@gmail.com>
2015-08-01 01:35:46 +0800

    modify code style

--- src/main/webapp/resources/js/common/redirect.js
+++ src/main/webapp/resources/js/common/redirect.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-	
-	// 點擊取消按鈕
-	$('#btn-cancel').on('click', redirectToHomePage);
-	
-	// 點擊註冊按鈕
-	$('#btn-register').on('click', redirectToRegisterPage);
-	
+$(document).ready(function() {
+
+    // 點擊取消按鈕
+    $('#btn-cancel').on('click', redirectToHomePage);
+
+    // 點擊註冊按鈕
+    $('#btn-register').on('click', redirectToRegisterPage);
+
 }); // end ready
 
 // 導向首頁
 function redirectToHomePage() {
-	window.location = '/';
+    window.location = '/';
 };
 
 // 導向註冊頁
 function redirectToRegisterPage() {
-	window.location = '/Register';
+    window.location = '/Register';
 };
