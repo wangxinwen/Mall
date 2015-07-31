a16c1fd5cd538b60775977ebb0d56d1ce870884a
RoySu1215 <roysu1215@gmail.com>
2015-08-01 01:22:01 +0800

    modify code style

--- src/main/java/tw/roysu/mall/service/IUserService.java
+++ src/main/java/tw/roysu/mall/service/IUserService.java
@@ -9,14 +9,14 @@ import tw.roysu.mall.exception.DataCheckException;
 public interface IUserService {
 	
 	/**
-	 * 會員註冊<br><br>
-	 * 
-	 * 如果帳號、暱稱或信箱已被申請過會拋出例外
-	 * 
-	 * @param user
-	 *        {@link User}
-	 * @throws DataCheckException
-	 */
-	public void register(User user) throws DataCheckException;
+     * 會員註冊<br><br>
+     * 
+     * 如果帳號、暱稱或信箱已被申請過會拋出例外
+     * 
+     * @param user
+     *        {@link User}
+     * @throws DataCheckException
+     */
+    public void register(User user) throws DataCheckException;
 
 }
