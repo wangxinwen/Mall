a750d650507ef0f93a421d44aa1b7763c4bf22d4
RoySu1215 <roysu1215@gmail.com>
2015-08-03 22:44:36 +0800

    logout function

---
+++ src/main/java/tw/roysu/mall/controller/UserController.java
@@ -0,0 +1,30 @@
+package tw.roysu.mall.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import tw.roysu.mall.constant.View;
+import tw.roysu.mall.utils.HttpSessionUtils;
+
+/**
+ * 會員Controller
+ */
+@Controller
+@RequestMapping(value = "/User")
+public class UserController {
+    
+    /**
+     * 登出
+     */
+    @RequestMapping(value = "/Logout", method = RequestMethod.GET)
+    public String logout(Model model, HttpSession session) {
+        HttpSessionUtils.removeUser(session);
+        model.addAttribute("msg", "登出成功");
+        return View.SUCCESS;
+    }
+
+}
